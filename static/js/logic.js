54d09b619877db7a55471c0e02ba1dd5255f6421
Morgan <mcordray88@protonmail.com>
2020-02-12 18:19:36 -0700

    changed circle size and color, added tooltip and legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -19,23 +19,78 @@ var map = L.map("map", {
   backgroundmap.addTo(map);
 
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(thedata){
-    function createColor(mag){
-        if (mag != 0){
-            return "#ff3333"
+function createStyle(feature){
+    return {
+        fillColor: createColor(feature.properties.mag),
+        color: feature.properties.color, 
+        radius: multiMag(feature.properties.mag)
+    }
+}
+
+
+
+
+
+function multiMag(mag){
+    if (mag == 0 ){
+        return 1
+    }
+    else {
+        return mag*5
+    }
+}
+
+function createColor(mag){
+        
+        if (mag > 5){
+            return "#FF0000"
+        }
+        if (mag > 4){
+            return "#B45F04"
+        }
+        if (mag > 3){
+            return "#FACC2E"
+        }
+        if (mag > 2){
+            return "#FFFF00"
+        }
+        if (mag > 1){
+            return "#BFFF00"
+        }
+        else {
+            return "#6E6E6E"
         }
     }
 
     L.geoJSON(thedata, {
         pointToLayer: function(feature, latlng) {
-            return L.marker(latlng);
+            return L.circleMarker(latlng);
         },
-        style: function (feature) {
-            return {color: feature.properties.color,
-                fillColor: createColor(feature.properties.mag)}
-        },
-        onEachFeature: function (feature, layer) {layer.bindPopup()}
+        style: createStyle,
+        
+        onEachFeature: function (feature, layer) {layer.bindPopup("Location: " + feature.properties.place + "<br>Mag: " + feature.properties.mag)}
     }).addTo(map);
 
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function () {
+
+    var div = L.DomUtil.create('div', 'info legend');
+    var grades = [0, 1, 2, 3, 4, 5];
+    var colors = ["#6E6E6E", "#BFFF00", "FFFF00", "#FACC2E", "#B45F04", "#FF0000"];
+
+    
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + colors[i] + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+}
+
+return div;
+};
+
+legend.addTo(map);
 
 })
     
